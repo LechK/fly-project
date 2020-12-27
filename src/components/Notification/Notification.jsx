27c1cc5f85804ddd2e72aec23ffca225dd619c83
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-27 17:59:43 +0200

    Notification with close button

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import * as S from "./Notification.style";
 
-function Notification({ children, color }) {
-  return <S.Notification color={color}>{children}</S.Notification>;
+function Notification({ children, color, handleChange }) {
+  return (
+    <S.Box>
+      <S.Notification color={color}>
+        {children}
+        <S.NotificationButton onClick={handleChange}>X</S.NotificationButton>
+      </S.Notification>
+    </S.Box>
+  );
 }
 
 export default Notification;
--- src/components/Notification/Notification.style.js
+++ src/components/Notification/Notification.style.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
+export const Box = styled.div`
+  padding: 0;
+`;
+
 export const Notification = styled.div`
+  font-size: 1em;
+  color: white;
   background-color: ${(props) =>
     props.color === "success"
       ? props.theme.lightGreen
@@ -9,4 +15,18 @@ export const Notification = styled.div`
   color: white;
   padding: 1em;
   font-family: "Alata", sans-serif;
+`;
+
+export const NotificationButton = styled.div`
+  border-radius: 50%;
+  position: sticky;
+  float: right;
+  margin-top: -0.5em;
+  width: 23px;
+  text-align: center;
+  height: 18px;
+  border: none;
+  background-color: black;
+  color: white;
+  cursor: pointer;
 `;
