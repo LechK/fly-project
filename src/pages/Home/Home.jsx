dd512295f5d40ebc516ccad327951a56eb4b80fa
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-12-27 17:59:24 +0200

    Calculate section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { Button, Section, Notification, Input } from "../../components";
+import React, { useState } from "react";
+import {
+  Button,
+  Section,
+  Notification,
+  Input,
+  Radio,
+  RCslider,
+} from "../../components";
 import * as S from "./Home.style";
 
 function Home() {
+  const [number, setNumber] = useState(1);
+  const [people, setPeople] = useState(1);
+  const [option, setOption] = useState("no");
+
+  const [notification, setNotification] = useState();
+
+  //function to calculate savings, on option value, if they are clients, or they are not
+  function calculate() {
+    if (option === "yes") {
+      return <S.H1>${people * number * 26}</S.H1>;
+    } else {
+      return <S.H1>$0</S.H1>;
+    }
+  }
+
   return (
     <>
-      <Notification color="success">
-        <strong>&#10003;</strong> Your Airport Confirmed!
+      <Notification color="success" handleChange={() => setNotification(false)}>
+        &#10003; Your Airport Confirmed!
       </Notification>
       <S.SubscribeSection>
         <S.Heading>Grab Flight Deals While They're Hot</S.Heading>
@@ -26,9 +48,57 @@ function Home() {
         <S.Wave />
       </S.SubscribeSection>
       <Section>
-        <Button color="secondary" handleClick={() => alert("Hi")}>
-          Check React out
-        </Button>
+        <S.H2>Calculate Your Savings!</S.H2>
+        <S.CalculationBox>
+          <S.CalcHeading>
+            How many times do you plan to travel in the next 12 months?
+          </S.CalcHeading>
+          <RCslider
+            min={0}
+            max={12}
+            value={number}
+            onChange={(e) => setNumber(e)}
+            minusClick={() => setNumber(number - 1)}
+            plusClick={() => setNumber(number + 1)}
+          />
+          <S.CalcHeading>
+            How many people will be travelling (including you)?
+          </S.CalcHeading>
+          <RCslider
+            min={1}
+            max={9}
+            value={people}
+            onChange={(e) => setPeople(e)}
+            minusClick={() => setPeople(people - 1)}
+            plusClick={() => setPeople(people + 1)}
+          />
+          <S.CalcHeading>Are you a member of SeatHustler.com?</S.CalcHeading>
+          <div>
+            <Radio
+              options={[
+                {
+                  id: "yes",
+                  name: "flight",
+                  value: "yes",
+                  text: "Yes, I love cheap flights",
+                },
+                {
+                  id: "no",
+                  name: "flight",
+                  value: "no",
+                  text: "No, I don't",
+                },
+              ]}
+              handleChange={(e) => setOption(e.target.value)}
+            />
+          </div>
+          <S.Line />
+          <S.H2>Your total savings</S.H2>
+          {calculate()}
+          <Button color="secondary">
+            <strong>&#10003;</strong>Start Saving
+          </Button>
+        </S.CalculationBox>
       </Section>
     </>
   );
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -47,3 +47,47 @@ export const Wave = styled.div`
   @media (min-width: ${({ theme }) => theme.mobile}) {
   }
 `;
+
+export const H2 = styled.h2`
+  color: ${({ theme }) => theme.textBlue};
+  font-size: 1.9em;
+  font-weight: lighter;
+  margin: 0 0 0.5em 0;
+`;
+
+export const CalculationBox = styled.div`
+  background-color: ${({ theme }) => theme.calcBox};
+  margin-bottom: 10em;
+  border-radius: 5px;
+  padding: 0 1.2em;
+  border: 1px solid ${({ theme }) => theme.lightBlue};
+  && Button {
+    padding: 0.6em 2.2em;
+    margin: 1em 0;
+    font-weight: lighter;
+  }
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    width: 500px;
+    margin: 0 auto;
+  }
+`;
+
+export const CalcHeading = styled.h5`
+  color: ${({ theme }) => theme.darkOrange};
+  font-weight: lighter;
+  font-size: large;
+`;
+
+export const Line = styled.div`
+  margin: 1em auto;
+  width: 60%;
+
+  border-top: 2px solid ${({ theme }) => theme.textBlue};
+`;
+
+export const H1 = styled.h1`
+  color: ${({ theme }) => theme.textBlue};
+  margin: 0;
+  font-size: 2.7em;
+  font-weight: lighter;
+`;
